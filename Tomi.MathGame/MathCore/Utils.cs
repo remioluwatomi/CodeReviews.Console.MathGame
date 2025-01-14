b793cadd55b462cc98fd0059e62c950f88f19438
tomi <remioluwatomicephas@gmail.com>
2025-01-14 06:00:36 +0100

    Tomi.MathGame Main Utils implementation methods added

--- Tomi.MathGame/MathCore/Utils.cs
+++ Tomi.MathGame/MathCore/Utils.cs
@@ -26,26 +26,82 @@ namespace MathCore
 
         public static bool ContinueGame()
         {
-            //infinite loop - this is just to bootstrap the project skeleton
-            return true;
+            string? keepPlaying = null;
+            Console.Write("Wanna play math game? y/n: ");
+
+
+            while (true)
+            {
+                keepPlaying = Console.ReadLine()?.Trim().ToLower();
+
+                if (string.IsNullOrWhiteSpace(keepPlaying))
+                {
+                    Console.WriteLine("Input cannot be empty. Please enter 'y' or 'n'.");
+                }
+
+
+                if (keepPlaying != "y" && keepPlaying != "n") Console.Write("Please enter 'y' or 'n': ");
+                else break;
+
+
+            }
+
+            return "y" == keepPlaying;
 
         }
 
         public static string PresentGameMenu()
         {
             Console.Clear();
-            return "Presenting Game Menu";
+            string? response = null;
+            while (true)
+            {
+                Console.WriteLine("Choose an operation: ");
+                Console.WriteLine("1. Addition (+)");
+                Console.WriteLine("2. Subtraction (-)");
+                Console.WriteLine("3. Multiplication (*)");
+                Console.WriteLine("4. Division (/)");
+                Console.WriteLine("5. Random Mode");
+                Console.WriteLine("6. View History");
+                Console.Write("Enter the number corresponding to the operation (1/2/3/4/5/6) or 'q' to quit: ");
+
+                response = Console.ReadLine()?.Trim().ToLower();
+                if (!string.IsNullOrWhiteSpace(response)) break;
+
+                Console.WriteLine("Input cannot be empty. Please select one of the operations");
+
+            }
+            return response;
         }
 
         public static string GetQuestion((int firstOperand, int secondOperand) operands, string operation)
         {
-            return "Util method to return a question";
+            return $"{operands.firstOperand} {operation} {operands.secondOperand} = ___";
         }
 
         public static int PromptForInt(string prompt)
         {
-            return 0;
+            while (true)
+            {
+                string? userAnswer = null;
+                Console.WriteLine($"\n{prompt}");
+
+                userAnswer = Console.ReadLine()?.Trim();
+                if (string.IsNullOrWhiteSpace(userAnswer))
+                {
+                    Console.WriteLine("You didn't input an answer..");
+                    continue;
+                }
+
+
+                if (int.TryParse(userAnswer, out int result))
+                {
+                    return result;
+                }
+                Console.WriteLine("Your answer must be a number..");
+            }
         }
+
 
     }
 }
